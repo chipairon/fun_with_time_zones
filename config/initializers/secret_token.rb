87570b397593d5f80527c5905b36663c52a4eb99
Chipairon <chipairon@fake.server>
2013-03-19 17:03:16 +0100

    inital project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunWithTimeZones::Application.config.secret_token = '727bbf0c205da5664116c502c8242ba2fb9037fe3d6a6618ee70bd85589accda4e5190106e43134e42ad25efb0f9fda8d7bf3400c2594225c6b55c3eed34a188'
